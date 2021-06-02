33999f9c006a852b60ae5ad4bf690c9a9655e907
together4eva <flap04@naver.com>
2021-06-02 22:51:00 +0900

    Study0602

---
+++ Study0602/Arrayappp.java
@@ -0,0 +1,19 @@
+public class Arrayappp {
+
+	public static void main(String[] args) {
+		
+		String[] users= new String[3];
+		
+		users[0]="AAA";
+		users[1]="BBB";
+		users[2]="CCC";
+		
+		System.out.println(users[0]);
+		System.out.println(users.length);
+		
+		int[] scores = {10, 100, 1000};
+		
+		System.out.println(scores[0]);
+		System.out.println(scores.length);
+	}
+}
---
+++ Study0602/LoopArraYYY.java
@@ -0,0 +1,15 @@
+
+public class LoopArraYYY {
+
+	public static void main(String[] args) {
+
+		String[] users = new String[3];
+		users[0] = "AAA";
+		users[1] = "BBB";
+		users[2] = "CCC";
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("<li>" + users[i] + "</li>");
+		}
+	}
+}
